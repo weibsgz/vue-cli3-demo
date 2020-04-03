591ad3426146d1a34b136c75f547e85b9b05120e
weibsgz <23004532@qq.com>
2020-04-03 14:12:32 +0800

    1

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,60 +1,87 @@
-import axios from 'axios'
-import { Message } from 'element-ui'
-import store from '@/store'
-import Cookies from 'js-cookie'
+import axios from "axios";
+import { Message } from "element-ui";
+import store from "@/store";
+import Cookies from "js-cookie";
 
-const baseURL = process.env.NODE_ENV == 'development' ? '/api/' : process.env.VUE_APP_BASE_API
+const baseURL = process.env.NODE_ENV == "development" ? "/api/" : process.env.VUE_APP_BASE_API;
 // create an axios instance
 const service = axios.create({
   baseURL,
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 10000 // request timeout
-})
+});
+
+const getRequestIdentify = (config, isReuest = false) => {
+  let url = config.url;
+  if (isReuest) {
+    url = config.baseURL + config.url.substring(1, config.url.length);
+  }
+  return config.method === "get"
+    ? encodeURIComponent(url + JSON.stringify(config.params))
+    : encodeURIComponent(config.url + JSON.stringify(config.data));
+};
+
+const pending = {};
+const CancelToken = axios.CancelToken;
+const removePending = (key, isRequest = false) => {
+  if (pending[key] && isRequest) {
+    pending[key]("取消重复请求");
+  }
+  delete pending[key];
+};
 
 // request interceptor
 service.interceptors.request.use(
   config => {
     // do something before request is sent
+    // 拦截重复请求(即当前正在进行的相同请求)
+    const requestData = getRequestIdentify(config, true); // 标识请求
+    removePending(requestData, true); // 取消重复请求
+    config.cancelToken = new CancelToken(c => {
+      // 创建当前请求的取消方法
+      pending[requestData] = c;
+    });
 
     if (store.getters.token) {
-      config.headers['X-Token'] = Cookies.get('token')
+      config.headers["X-Token"] = Cookies.get("token");
     }
-    return config
+    return config;
   },
   error => {
     // do something with request error
-    console.log(error) // for debug
-    return Promise.reject(error)
+    console.log(error); // for debug
+    return Promise.reject(error);
   }
-)
+);
 
 // response interceptor
 service.interceptors.response.use(
   response => {
-    const res = response.data
+    const res = response.data;
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.hasOwnProperty('status') && res.status !== 1000) {// 发布视频获取token接口响应数据格式错误添加的 status 属性判断条件  
+    if (res.hasOwnProperty("status") && res.status !== 1000) {
+      // 发布视频获取token接口响应数据格式错误添加的 status 属性判断条件
       Message({
-        message: res.message || 'Error',
-        type: 'error',
+        message: res.message || "Error",
+        type: "error",
         duration: 5 * 1000
-      })
+      });
 
-      return Promise.reject(new Error(res.message || 'Error'))
+      return Promise.reject(new Error(res.message || "Error"));
     } else {
-      return res
+      return res;
     }
   },
   error => {
-    console.log('err' + error) // for debug
+    console.log("err" + error); // for debug
     Message({
       message: error.message,
-      type: 'error',
+      type: "error",
       duration: 5 * 1000
-    })
-    return Promise.reject(error)
+    });
+    return Promise.reject(error);
   }
-)
+);
 
-export default service
+export default service;
--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,36 @@
-const path = require('path')
-const pck = require('./package.json')
+const path = require("path");
+const pck = require("./package.json");
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
-const name = pck.name // page title
+const name = pck.name; // page title
 
-const port = process.env.port || process.env.npm_config_port || 9527 // dev port
+const port = process.env.port || process.env.npm_config_port || 9527; // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
+  // 项目部署的基础路径
+  // 我们默认假设你的应用将会部署在域名的根部，
+  // 比如 https://www.my-app.com/
+  // 如果你的应用时部署在一个子路径下，那么你需要在这里
+  // 指定子路径。比如，如果你的应用部署在
+  // https://www.foobar.com/my-app/
+  // 那么将这个值改为 `/my-app/`
+  // 基本路径 baseURL已经过时
+  publicPath: "/",
+  outputDir: "dist",
+  // 静态资源目录 (js, css, img, fonts)
+  assetsDir: "static",
   // lintOnSave: process.env.NODE_ENV === 'development',
+  // // eslint-loader 是否在保存的时候检查，编译不规范时，设为true在命令行中警告，若设为error则不仅警告，并且编译失败
   lintOnSave: false,
+  // 生产环境是否生成 sourceMap 文件，默认true，若不需要生产环境的sourceMap，可以设置为false，加速生产环境的构建
   productionSourceMap: false,
+
+  // 是否启用dll See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#dll-mode
+  dll: false,
   devServer: {
     port: port,
     open: true,
@@ -32,99 +39,97 @@ module.exports = {
       errors: true
     },
     proxy: {
-      '^/api': {
+      "^/api": {
         target: process.env.VUE_APP_BASE, // 服务器api地址
         changeOrigin: true, // 是否跨域
         ws: true // proxy websockets
       }
     }
   },
+
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src')
+        "@": resolve("src")
       }
     }
   },
+  // 调整内部的 webpack 配置。查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli/webpack.md
   chainWebpack(config) {
     // 预加载
-    config.plugins.delete('preload')
-    config.plugins.delete('prefetch')
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
 
     // set svg-sprite-loader
     config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
+      .rule("svg")
+      .exclude.add(resolve("src/icons"))
+      .end();
     config.module
-      .rule('icons')
+      .rule("icons")
       .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
+      .include.add(resolve("src/icons"))
       .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
       .options({
-        symbolId: 'icon-[name]'
+        symbolId: "icon-[name]"
       })
-      .end()
+      .end();
 
     // set preserveWhitespace
     config.module
-      .rule('vue')
-      .use('vue-loader')
-      .loader('vue-loader')
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
       .tap(options => {
-        options.compilerOptions.preserveWhitespace = true
-        return options
+        options.compilerOptions.preserveWhitespace = true;
+        return options;
       })
-      .end()
+      .end();
 
     config
       // https://webpack.js.org/configuration/devtool/#development
-      .when(process.env.NODE_ENV === 'development',
-        config => config.devtool('cheap-source-map')
-      )
+      .when(process.env.NODE_ENV === "development", config => config.devtool("cheap-source-map"));
 
-    config
-      .when(process.env.NODE_ENV !== 'development',
-        config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
+    config.when(process.env.NODE_ENV !== "development", config => {
+      config
+        .plugin("ScriptExtHtmlWebpackPlugin")
+        .after("html")
+        .use("script-ext-html-webpack-plugin", [
+          {
             // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
-          config
-            .optimization.splitChunks({
-              chunks: 'all',
-              cacheGroups: {
-                libs: {
-                  name: 'chunk-libs',
-                  test: /[\\/]node_modules[\\/]/,
-                  priority: 10,
-                  chunks: 'initial' // only package third parties that are initially dependent
-                },
-                elementUI: {
-                  name: 'chunk-elementUI', // split elementUI into a single package
-                  priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                },
-                commons: {
-                  name: 'chunk-commons',
-                  test: resolve('src/components'), // can customize your rules
-                  minChunks: 3, //  minimum common number
-                  priority: 5,
-                  reuseExistingChunk: true
-                }
-              }
-            })
-          config.optimization.runtimeChunk('single')
+            inline: /runtime\..*\.js$/
+          }
+        ])
+        .end();
+      config.optimization.splitChunks({
+        chunks: "all",
+        cacheGroups: {
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: "initial" // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: "chunk-elementUI", // split elementUI into a single package
+            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+          },
+          commons: {
+            name: "chunk-commons",
+            test: resolve("src/components"), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
+          }
         }
-      )
+      });
+      config.optimization.runtimeChunk("single");
+    });
   }
-}
+};
